d74e841f94957edb7fd74cadd628f01175f5d76f
Tanya Vasylovych <tetiana.vasylovych.work@gmail.com>
2025-10-18 11:17:26 +0300

    Upd errorHandler

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,6 +1,19 @@
-export default (err, req, res) => {
-  const status = err.status || 500;
-  res.status(status).json({
-    message: err.message || 'Internal Server Error',
+import { HttpError } from 'http-errors';
+
+export default (err, req, res, next) => {
+  console.error('Error Middleware:', err);
+
+  if (err instanceof HttpError) {
+    return res.status(err.status).json({
+      message: err.message || err.name,
+    });
+  }
+
+  const isProd = process.env.NODE_ENV === 'production';
+
+  res.status(500).json({
+    message: isProd
+      ? 'Something went wrong. Please try again later.'
+      : err.message,
   });
 };

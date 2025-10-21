1ff8af7a6b56351e62c09c0a3569648d867b9295
Tanya Vasylovych <tetiana.vasylovych.work@gmail.com>
2025-10-21 18:54:26 +0300

    validation

---
+++ src/validations/notesValidation.js
@@ -0,0 +1,44 @@
+import { Joi, Segments } from 'celebrate';
+import { isValidObjectId } from 'mongoose';
+const tags = require('../contacts/tags');
+
+const objectIdValidator = (value, helpers) => {
+  return !isValidObjectId(value) ? helpers.message('Invalid id format') : value;
+};
+export const getAllNotesSchema = {
+  [Segments.QUERY]: Joi.object({
+    page: Joi.number().integer().min(1).default(1),
+    perPage: Joi.number().integer().min(5).max(20).default(10),
+    tag: Joi.string().valid(...tags),
+    search: Joi.string().allow(''),
+  }),
+};
+
+export const noteIdSchema = {
+  [Segments.PARAMS]: Joi.object().keys({
+    noteId: Joi.string().custom(objectIdValidator).required(),
+  }),
+};
+
+export const createNoteSchema = {
+  [Segments.BODY]: Joi.object({
+    title: Joi.string().min(1).required(),
+    content: Joi.string().allow(''),
+    tag: Joi.string()
+      .valid(...tags)
+      .required(),
+  }),
+};
+
+export const updateNoteSchema = {
+  [Segments.PARAMS]: Joi.object().keys({
+    noteId: Joi.string().custom(isValidObjectId).required(),
+  }),
+  [Segments.BODY]: Joi.object().keys({
+    title: Joi.string().min(1).optional(),
+    content: Joi.string().allow('').optional(),
+    tag: Joi.string()
+      .valid(...tags)
+      .optional(),
+  }),
+};

68542bc4dc57e78e0f69e697afdeb6fae4662981
Tanya Vasylovych <tetiana.vasylovych.work@gmail.com>
2025-10-23 20:46:37 +0300

    upd tags

--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -1,6 +1,6 @@
 import { Joi, Segments } from 'celebrate';
 import { isValidObjectId } from 'mongoose';
-const tags = require('../contacts/tags');
+import { TAGS } from '../constants/tags.js';
 
 const objectIdValidator = (value, helpers) => {
   return !isValidObjectId(value) ? helpers.message('Invalid id format') : value;
@@ -9,7 +9,7 @@ export const getAllNotesSchema = {
   [Segments.QUERY]: Joi.object({
     page: Joi.number().integer().min(1).default(1),
     perPage: Joi.number().integer().min(5).max(20).default(10),
-    tag: Joi.string().valid(...tags),
+    tag: Joi.string().valid(...TAGS),
     search: Joi.string().allow(''),
   }),
 };
@@ -25,7 +25,7 @@ export const createNoteSchema = {
     title: Joi.string().min(1).required(),
     content: Joi.string().allow(''),
     tag: Joi.string()
-      .valid(...tags)
+      .valid(...TAGS)
       .required(),
   }),
 };
@@ -38,7 +38,7 @@ export const updateNoteSchema = {
     title: Joi.string().min(1).optional(),
     content: Joi.string().allow('').optional(),
     tag: Joi.string()
-      .valid(...tags)
+      .valid(...TAGS)
       .optional(),
   }),
 };

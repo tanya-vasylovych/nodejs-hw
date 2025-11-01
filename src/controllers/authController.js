a22c07eef025887ec35e6dd11d628e9c2e3f0884
Tanya Vasylovych <tetiana.vasylovych.work@gmail.com>
2025-11-01 13:47:43 +0200

    user

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -2,7 +2,7 @@ import { createSession, setSessionCookies } from '../services/auth.js';
 import { Session } from '../models/session.js';
 import createHttpError from 'http-errors';
 import bcrypt from 'bcrypt';
-import User from '../models/user.js';
+import { User } from '../models/user.js';
 
 export const registerUser = async (req, res, next) => {
   const { email, password } = req.body;
